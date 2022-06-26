61ab22be303c8cc14c1e7b79d55652b72033bba0
Dikshant <009Dikshant009@gmail.com>
2022-06-26 21:56:19 +0530

    added register and login functionality

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose  = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add a email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false,
+    }
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('User', userSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
-console.log('server');
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+const colors = require('colors');
+const userRoutes = require('./routes/userRoutes.js');
+const { errorHandler } = require('./middleware/errorMiddleware.js');
+
+// Connect to database
+const { connectDB } = require('./config/db.js');
+connectDB();
+
+// Using middleware to get the data which is passed by user in post request : to Parse data
+app.use(express.urlencoded({ extended: false }))
+
+// Parse json
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ sucess: true })
+})
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+    console.log('listening on port ' + process.env.PORT);
+})
